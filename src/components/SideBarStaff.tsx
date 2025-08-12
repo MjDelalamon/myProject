5435df2cf6705e5622af1451f1406dc951d91d22
Mj Delalamon <markjohndelalamon.com>
2025-08-12 23:14:04 +0800

    update

--- src/components/SideBarStaff.tsx
+++ src/components/SideBarStaff.tsx
@@ -10,25 +10,25 @@ function SidebarStaff() {
             Take Order
           </Link>
           <Link to="/customers" className="sidebar-link">
-            Customers
+            -
           </Link>
           <Link to="/orders" className="sidebar-link">
-            Orders
+            -
           </Link>
           <Link to="/rewards" className="sidebar-link">
-            Rewards
+            -
           </Link>
           <Link to="/menu" className="sidebar-link">
-            Menu
+            -
           </Link>
           <Link to="/wallet" className="sidebar-link">
-            Wallet
+            -
           </Link>
           <Link to="/feedback" className="sidebar-link">
-            Feedback
+            -
           </Link>
           <Link to="/notifications" className="sidebar-link">
-            Notifications
+            -
           </Link>
           <Link to="/settings" className="sidebar-link">
             Settings
