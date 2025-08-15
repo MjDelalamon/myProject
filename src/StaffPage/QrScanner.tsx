9d6cc940f48df044217a7ff23175d207f59b9091
Mj Delalamon <markjohndelalamon.com>
2025-08-15 22:15:23 +0800

    changes

--- src/StaffPage/QrScanner.tsx
+++ src/StaffPage/QrScanner.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { Html5QrcodeScanner } from "html5-qrcode";
+import { Link } from "react-router-dom";
 
 export default function QRScanner() {
   useEffect(() => {
@@ -31,8 +32,46 @@ export default function QRScanner() {
   }, []);
 
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <div id="reader" style={{ width: "300px" }}></div>
-    </div>
+    <>
+      <div className="sidebar">
+        <h2 className="sidebar-title">Staff Panel</h2>
+        <nav className="sidebar-nav">
+          <Link to="/qr-scanner" className="sidebar-link">
+            Scan QR
+          </Link>
+          <Link to="/take-order" className="sidebar-link">
+            Take Order
+          </Link>
+          <Link to="/orders" className="sidebar-link">
+            -
+          </Link>
+          <Link to="/rewards" className="sidebar-link">
+            -
+          </Link>
+          <Link to="/menu" className="sidebar-link">
+            -
+          </Link>
+          <Link to="/wallet" className="sidebar-link">
+            -
+          </Link>
+          <Link to="/feedback" className="sidebar-link">
+            -
+          </Link>
+          <Link to="/notifications" className="sidebar-link">
+            -
+          </Link>
+          <Link to="/settings" className="sidebar-link">
+            Settings
+          </Link>
+          <Link to="/" className="sidebar-link">
+            Logout
+          </Link>
+        </nav>
+      </div>
+
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div id="reader" style={{ width: "300px" }}></div>
+      </div>
+    </>
   );
 }
